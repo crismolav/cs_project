bf75903e9351e450b701eb471efacbf06df3cd42
crismolav <cmoraleso@utexas.edu>
2019-11-23 19:09:01 +0100

    baseline works

--- project/baseline.py
+++ project/baseline.py
@@ -6,8 +6,7 @@ from nltk.corpus import sentiwordnet as swn
 from nltk.corpus import wordnet as wn
 import numpy as np
 import math
-from sklearn.metrics import precision_score
-
+from sklearn.metrics import precision_score, recall_score, accuracy_score
 
 def process_reviews(star_rating_list, review_list):
     Y_true = list()
@@ -23,6 +22,16 @@ def process_reviews(star_rating_list, review_list):
         baseline_classification = get_baseline_classification(review=processed_review)
         Y_true.append(original_classification)
         Y_pred.append(baseline_classification)
+        if index == 100:
+            break
+
+    bl_precision = precision_score(Y_true, Y_pred)
+    bl_recall    = recall_score(Y_true, Y_pred)
+    bl_accuracy  = accuracy_score(Y_true, Y_pred)
+    print("Base line indicators\n")
+    print("Precision: %s" % bl_precision)
+    print("Recall: %s" % bl_recall)
+    print("Accuracy: %s"%bl_accuracy)
 
 def get_classification_group_for_star_rating(star_rating):
     if int(star_rating) in [4, 5]:
@@ -57,17 +66,15 @@ def get_sentiment_scores(review):
 def get_sentiment_score_word_pos_tuple(word_pos_tuple):
     word = word_pos_tuple[0]
     pos = word_pos_tuple[1]
-    try:
-        wordnet_pos = transform_pos_to_wordnet_notation(pos=pos)
-    except:
-        set_trace()
+    if not pos_is_in_model(pos):
+        print("WARNING: pos not recognized: %s"%pos)
+        return 0, 0
+
+    wordnet_pos = transform_pos_to_wordnet_notation(pos=pos)
     word_synsets = wn.synsets(word, wordnet_pos)
     pos_sentiment_score, neg_sentiment_score = \
         calculate_avg_sentiment_scores_for_synsets(word_synsets=word_synsets)
-    # if pos_sentiment_score != neg_sentiment_score:
-    #     set_trace()
-    # if word_pos_tuple == ('understand', 'ADJ'):
-    #     set_trace()
+
     return pos_sentiment_score , neg_sentiment_score
 
 def calculate_avg_sentiment_scores_for_synsets(word_synsets):
@@ -91,9 +98,28 @@ def calcualte_sentiment_scores_for_synset(synset):
 
     return pos_score, neg_score
 
+def get_pos_to_wordnet_pos_dictionary():
+    pos_to_wordnet_pos_dictionary = {
+        'NOUN': 'n',
+        'PRON': 'n',
+        'ADV': 'r',
+        'VERB': 'v',
+        'ADJ': 'a',
+        'NUM': 'a',
+        'DET': 'a',
+        'ADP': 'a',
+        'PRT': 'a',
+        'CONJ': 'a'
+    }
+    return pos_to_wordnet_pos_dictionary
+def pos_is_in_model(pos):
+    pos_to_wordnet_pos_dictionary = get_pos_to_wordnet_pos_dictionary()
+    return pos in pos_to_wordnet_pos_dictionary
+
 def transform_pos_to_wordnet_notation(pos):
     pos_to_wordnet_pos_dictionary = {
         'NOUN': 'n',
+        'PRON': 'n',
         'ADV': 'r',
         'VERB': 'v',
         'ADJ': 'a',
@@ -115,12 +141,4 @@ if __name__=="__main__":
     star_rating_list, review_list = load_csv_info("10000reviews.txt")
     # PROCESS REVIEWS
     process_reviews(star_rating_list=star_rating_list, review_list=review_list)
-
-    # SPLIT THE REVIEWS INTO POSITIVE (RATING 4,5) AND NEGATIVE (RATING 1,2)
-    reviews_12, reviews_45 = split_reviews(star_rating_list, review)
-    ######## NEGATIVE REVIEWS ########
-    # LOAD THE TEXT
-    negative_text = load_text('reviews12.txt')
-    # PRE-PROCESS
-    pre_processed = prepro.pre_process(negative_text)
 
