6a55ee9eb09197b3224e082fe4a1c1a42131a275
Andrea Valenzuela Ramírez <andreavalenzuelaramirez@poblenou-137-21.eduroamcp.upf.edu>
2020-01-07 15:16:22 +0100

    stars

--- project/helpers.py
+++ project/helpers.py
@@ -20,15 +20,25 @@ def load_text(filename):
 
 def split_reviews(star_rating_list, review):
     reviews_45 = []
+    star_45 = []
     reviews_12 = []
-    with open('reviews45.txt', 'w') as f45:
-        with open('reviews12.txt', 'w') as f12:
-            for ii, star in enumerate(star_rating_list):
-                current_review = review[ii]
-                if star == '4' or star == '5':
-                    reviews_45.append(current_review)
-                    f45.write(current_review + '\n')
-                elif star == '1' or star == '2':
-                    reviews_12.append(current_review)
-                    f12.write(current_review + '\n')
-    return reviews_12, reviews_45
+    star_12 = []
+    reviews1245 = []
+    star1245 = []
+
+    for ii, star in enumerate(star_rating_list):
+        current_review = review[ii]
+        if star == '4' or star == '5':
+            reviews_45.append(current_review)
+            star_45.append(star)
+            reviews1245.append(current_review)
+            star1245.append(star)
+        elif star == '1' or star == '2':
+            reviews_12.append(current_review)
+            star_12.append(star)
+            reviews1245.append(current_review)
+            star1245.append(star)
+
+    return reviews_12, reviews_45, reviews1245, star_12, star_45, star1245
+
+
